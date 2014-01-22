965146f2354eaa80c5db525117355ffc5b98c982
supernullset <sean.j.williamson@gmail.com>
2014-01-21 20:29:03 -0800

    lein project of simple euler solutions

---
+++ clojure-euler/project.clj
@@ -0,0 +1,11 @@
+(defproject clojure-euler "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dev-dependencies [[net.mikera/core.matrix "0.18.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.5.1"]
+                 [net.mikera/core.matrix "0.18.0"]
+                 [criterium "0.4.2"]
+                 ])
