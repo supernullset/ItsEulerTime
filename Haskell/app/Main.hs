35091fe12648ad67ccdfb4e120e69cea170d6e6c
supernullset <sean.j.williamson@gmail.com>
2015-07-05 15:10:11 -0700

    update main with mapM

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -1,16 +1,16 @@
 module Main where
 
-import Lib
 import Problems as P
-import Criterion.Main
+import Control.Monad
 
-main = defaultMain [
-   bgroup "Problems" [ bench "001" $ whnf P.zeroZeroOne ()
-                     , bench "002" $ whnf P.zeroZeroTwo ()
-                     , bench "003" $ whnf P.zeroZeroThree ()
-                     , bench "004" $ whnf P.zeroZeroFour ()
-                     , bench "005" $ whnf P.zeroZeroFive ()
-                     , bench "006" $ whnf P.zeroZeroSix ()
-                     , bench "007" $ whnf P.zeroZeroSeven ()
-                     ]
-   ]
+main = do
+  mapM_ (\(x,y) -> putStrLn $ "Problem " ++ x ++ ": " ++ y) [
+      ("001", show $ P.zeroZeroOne ())
+    , ("002", show $ P.zeroZeroTwo ())
+    , ("003", show $ P.zeroZeroThree ())
+    -- , ("004", P.zeroZeroFour ())
+    -- , ("005", P.zeroZeroFive ())
+    -- , ("006", P.zeroZeroSix ())
+    -- , ("007", P.zeroZeroSeven ())
+    ]
+
