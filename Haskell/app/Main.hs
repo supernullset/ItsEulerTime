074a454d7ea5100af9061d4bc002cf8b347178c1
supernullset <sean.j.williamson@gmail.com>
2015-07-05 15:28:14 -0700

    figure out way to remove extraneous () from solutions

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -5,10 +5,10 @@ import Control.Monad
 
 main = do
   mapM_ (\(x,y) -> putStrLn $ "Problem " ++ x ++ ": " ++ y) [
-      ("001", show $ P.zeroZeroOne ())
-    , ("002", show $ P.zeroZeroTwo ())
-    , ("003", show $ P.zeroZeroThree ())
-    -- , ("004", P.zeroZeroFour ())
+      ("001", show $ P.zeroZeroOne)
+    , ("002", show $ P.zeroZeroTwo)
+    , ("003", show $ P.zeroZeroThree)
+    , ("004", show $ P.zeroZeroFour)
     -- , ("005", P.zeroZeroFive ())
     -- , ("006", P.zeroZeroSix ())
     -- , ("007", P.zeroZeroSeven ())
--- Haskell/app/MainBench.hs
+++ Haskell/app/MainBench.hs
@@ -5,12 +5,12 @@ import Problems as P
 import Criterion.Main
 
 main = defaultMain [
-   bgroup "Problems" [ bench "001" $ whnf P.zeroZeroOne ()
-                     , bench "002" $ whnf P.zeroZeroTwo ()
-                     , bench "003" $ whnf P.zeroZeroThree ()
-                     , bench "004" $ whnf P.zeroZeroFour ()
-                     , bench "005" $ whnf P.zeroZeroFive ()
-                     , bench "006" $ whnf P.zeroZeroSix ()
-                     , bench "007" $ whnf P.zeroZeroSeven ()
+   bgroup "Problems" [ bench "001" $ whnf (\x -> P.zeroZeroOne) ()
+                     , bench "002" $ whnf (\x -> P.zeroZeroTwo) ()
+                     , bench "003" $ whnf (\x -> P.zeroZeroThree) ()
+                     , bench "004" $ whnf (\x -> P.zeroZeroFour) ()
+                     , bench "005" $ whnf (\x -> P.zeroZeroFive) ()
+                     , bench "006" $ whnf (\x -> P.zeroZeroSix) ()
+                     , bench "007" $ whnf (\x -> P.zeroZeroSeven) ()
                      ]
    ]
