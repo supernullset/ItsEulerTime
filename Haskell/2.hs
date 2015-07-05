c96f35ef76ca8dcb6c752469417e2f0c72838937
supernullset <sean.j.williamson@gmail.com>
2015-07-05 14:35:36 -0700

    apparently much has changed

--- Haskell/2.hs
+++
@@ -1,7 +0,0 @@
-module Main where
-
-main :: IO()
-main =
-    print (sum [x | x <- takeWhile (<= 4000000) fibs, even x])
-        where
-          fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
--- Haskell/7.hs
+++
@@ -1,6 +0,0 @@
-module Main where
-
-import Data.Numbers.Primes
-
-main = do
-  print (primes !! 10000)
