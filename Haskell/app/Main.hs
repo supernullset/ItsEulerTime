478cbd8df982b5c4ac5ee40038e0c3d9e5ecd8d0
supernullset <sean.j.williamson@gmail.com>
2015-07-05 15:41:37 -0700

    Int to 3d format string

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -2,15 +2,18 @@ module Main where
 
 import Problems as P
 import Control.Monad
+import Text.Printf (printf)
 
 main = do
-  mapM_ (\(x,y) -> putStrLn $ "Problem " ++ x ++ ": " ++ y) [
-      ("001", show $ P.zeroZeroOne)
-    , ("002", show $ P.zeroZeroTwo)
-    , ("003", show $ P.zeroZeroThree)
-    , ("004", show $ P.zeroZeroFour)
-    , ("005", show $ P.zeroZeroFive)
-    , ("006", show $ P.zeroZeroSix)
-    , ("007", show $ P.zeroZeroSeven)
+  mapM_ (\(x,y) -> putStrLn $ "Problem " ++ (numToStr x) ++ ": " ++ y) [
+      (1, show $ P.zeroZeroOne)
+    , (2, show $ P.zeroZeroTwo)
+    , (3, show $ P.zeroZeroThree)
+    , (4, show $ P.zeroZeroFour)
+    , (5, show $ P.zeroZeroFive)
+    , (6, show $ P.zeroZeroSix)
+    , (7, show $ P.zeroZeroSeven)
     ]
 
+numToStr :: Int -> String
+numToStr a = printf "%03d" a
