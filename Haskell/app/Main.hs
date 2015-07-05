f8fe7d7dcf7990bfe7414a7d03e8a6945d1f4d4c
supernullset <sean.j.williamson@gmail.com>
2015-07-05 16:41:19 -0700

    support dynamic solving of euler problems

--- Haskell/app/Main.hs
+++ Haskell/app/Main.hs
@@ -3,17 +3,34 @@ module Main where
 import Problems as P
 import Control.Monad
 import Text.Printf (printf)
+import qualified Data.Map.Lazy as Map
+import System.Environment as Sys
+import System.Exit as E
+
+problems = Map.fromList [ (1, show $ P.zeroZeroOne)
+                        , (2, show $ P.zeroZeroTwo)
+                        , (3, show $ P.zeroZeroThree)
+                        , (4, show $ P.zeroZeroFour)
+                        , (5, show $ P.zeroZeroFive)
+                        , (6, show $ P.zeroZeroSix)
+                        , (7, show $ P.zeroZeroSeven)
+           ]
 
 main = do
-  mapM_ (\(x,y) -> putStrLn $ "Problem " ++ (numToStr x) ++ ": " ++ y) [
-      (1, show $ P.zeroZeroOne)
-    , (2, show $ P.zeroZeroTwo)
-    , (3, show $ P.zeroZeroThree)
-    , (4, show $ P.zeroZeroFour)
-    , (5, show $ P.zeroZeroFive)
-    , (6, show $ P.zeroZeroSix)
-    , (7, show $ P.zeroZeroSeven)
-    ]
+  args <- Sys.getArgs
+  if length args < 1
+    then do
+      putStrLn "Please provide an integer argument"
+      E.exitFailure
+    else do
+      if args == ["--all"]
+        then do
+          putStrLn "No support for all problems yet"
+          E.exitSuccess
+        else do
+          let requestedProblems = map (\x -> read x :: Int) args
+          let problemsToSolve = map (\x -> (x, problems Map.! x)) requestedProblems
+          mapM_ (\(x,y) -> putStrLn $ "Problem " ++ (numToStr x) ++ ": " ++ y) problemsToSolve
 
 numToStr :: Int -> String
 numToStr a = printf "%03d" a
