17b30f3d68ad3d9ad5ad3e2671bb3d9bdcc63e6b
supernullset <sean.j.williamson@gmail.com>
2015-07-06 10:26:05 -0700

    add specs for 001

--- Haskell/test/Spec.hs
+++ Haskell/test/Spec.hs
@@ -1,2 +1,10 @@
+import Test.Hspec
+import qualified ZeroZeroOne (main)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "001" $ do
+    it "must yield the correct answer" $
+      ZeroZeroOne.main `shouldBe` 233168
+
+
