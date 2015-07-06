8e088798b545c3fc1909912e31347ba576620708
supernullset <sean.j.williamson@gmail.com>
2015-07-06 10:35:43 -0700

    Add specs

--- Haskell/test/Spec.hs
+++ Haskell/test/Spec.hs
@@ -1,10 +1,27 @@
 import Test.Hspec
+
 import qualified ZeroZeroOne (main)
+import qualified ZeroZeroTwo (main)
+import qualified ZeroZeroThree (main)
+import qualified ZeroZeroFour (main)
+import qualified ZeroZeroFive (main)
+import qualified ZeroZeroSix (main)
+import qualified ZeroZeroSeven (main)
 
 main :: IO ()
 main = hspec $ do
   describe "001" $ do
-    it "must yield the correct answer" $
+    it "must yield the correct answer of 233168" $
       ZeroZeroOne.main `shouldBe` 233168
 
+  describe "002" $ do
+    it "must yield the correct answer of 4613732" $
+      ZeroZeroTwo.main `shouldBe` 4613732
 
+  describe "003" $ do
+    it "must yield the correct answer of 6857" $
+      ZeroZeroThree.main `shouldBe` 6857
+
+  describe "004" $ do
+    it "must yield the correct answer of 906609" $
+      ZeroZeroFour.main `shouldBe` 906609
