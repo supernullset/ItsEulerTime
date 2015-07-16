f6a36a79f02e1924fb3025dafadf1e26a4c611e8
supernullset <sean.j.williamson@gmail.com>
2015-07-16 15:00:24 -0700

    this is hacky/dirty 022

--- Haskell/src/ZeroTwoTwo.hs
+++ Haskell/src/ZeroTwoTwo.hs
@@ -1,6 +1,16 @@
 module ZeroTwoTwo (main) where
 
-main :: IO String
-main = do
-  let retVal = 0
-  return $ show retVal
+import System.IO.Unsafe (unsafePerformIO)
+import System.IO (readFile)
+import qualified Data.Map.Strict as Map
+import Data.List (sort)
+
+solution = do
+  let numVals = Map.fromList $ zip ['A'..'Z'] [1..26]
+  names <- readFile "src/extras/022.txt"
+  let ns = sort $ read (['['] ++ names ++ [']']) :: [String]
+  let intNs = map (\l -> sum $ map (\x -> numVals Map.! x) l) ns
+  return $ sum $ zipWith (*) intNs [1..]
+
+-- TODO: This is super bad
+main = unsafePerformIO solution
