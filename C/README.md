b11955b690cbee5b3a5dc59844ff291eb0d5309c
supernullset <sean.j.williamson@gmail.com>
2021-01-20 14:52:53 -0800

    add solution for 001

--- C/README.md
+++ C/README.md
@@ -1,3 +1,11 @@
 Hey look - its project Euler in raw C!!
 
 This will be interesting...
+
+Here is what I am thinking - I want to develop each solution
+independently for maybe the first 10 problems. After that, I want to
+build a CLI application which will have a way to cache values that
+each executable can rely on - Prim numbers come to mind, and maybe
+some of the pyramidal/triplet computations which are reused in later
+problems. It would be very cool if I could develop a little REPL for
+interacting with the solutions and interactively executing them.
--- C/src/001.c
+++ C/src/001.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+/*********************************************************************/
+/* If we list all the natural numbers below 10 that are multiples of */
+/* 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.   */
+/*                                                                   */
+/* Find the sum of all the multiples of 3 or 5 below 1000.           */
+/*********************************************************************/
+
+
 int main(void)
 {
-    printf("Hello, World!\n");
+  // Ensure this is appropriately initialized rather than being a garbage number
+  int sum = 0;
+
+  // Brute force search - there is an additonal perf improvement that
+  // we could hit here by some basic math about what numbers are
+  // possible candidates 2021-01-20:14:48
+  for (int i = 0; i < 1000; ++i) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+
+  printf("%i\n", sum);
+  return 0;
 }
